5c7ded349c7509fed44e092e0222331d6c032405
Steven <theepicsnail@gmail.com>
2011-11-26 05:21:09 -0500

    Removed logging, and cleaned up a little.

--- Plugins/Foo.py
+++ Plugins/Foo.py
@@ -1,14 +1,16 @@
 from Yakr.Action import *
-from Yakr.Logger import logger
-logger.debug("Inside plugin.")
+
 def handle_line(line):
+    #unpack the args. This might be doable easier
     prefix,command,args = line["prefix"],line["command"],line["args"]
 
     if command=="PRIVMSG":
-        if args[1].startswith("!repeat"):
-            Say(args[0],"<{}>".format(args[1].replace("!repeat","")))
+        if args[1].startswith("!repeat "):
+            Say(args[0],"<{}>".format(args[1].replace("!repeat ","")))
+
         if args[1] == "!part":
             Part(args[0])
+
     if command=="INVITE":
         Join(args[1])
 
