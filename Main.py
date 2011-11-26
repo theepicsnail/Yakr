e55b144168d178f2fe95fd959b5e9f4c7f16dcb4
Steven <theepicsnail@gmail.com>
2011-11-26 06:12:16 -0500

    Added a simple plugin testing tool.

--- Main.py
+++ Main.py
@@ -1,19 +1,23 @@
 from Yakr.Irc import Bot
+import sys
+SETTINGS = {
+    'server': 'udderweb.com', 
+    'nick': 'Dot', 
+    'realname': 'Dot the bot', 
+    'port': 6667, 
+    'ssl': False, 
+    'channels': ['#test'], 
+    'pluginPath': 'Plugins',
+    }
 
 if __name__ == '__main__':
-    
-    SETTINGS = {
-        'server': 'udderweb.com', 
-        'nick': 'Dot', 
-        'realname': 'Dot the bot', 
-        'port': 6667, 
-        'ssl': False, 
-        'channels': ['#test'], 
-        'pluginPath': 'Plugins',
-        }
-    
-    bot = Bot(SETTINGS)
-    bot.start()
+    if len(sys.argv)==1:
+        bot = Bot(SETTINGS)
+        bot.start()
+    elif sys.argv[1]=="PluginTest":
+        from Yakr.Plugin import PluginManager
+        PluginManager(SETTINGS["pluginPath"]).autoload()
+
 #    jobs = [gevent.spawn(bot.start)]
 #    gevent.joinall(jobs)
 
