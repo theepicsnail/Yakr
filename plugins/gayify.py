0923d92ddb901a9ddb46c69cf953fbe726ae3977
Steven <theepicsnail@gmail.com>
2013-08-29 03:08:02 -0400

    Added gayify plugin

---
+++ plugins/gayify.py
@@ -0,0 +1,8 @@
+from . import *
+from random import randint
+@command("gayify")
+def gayify(who, what, where):
+    out = ""
+    for c in what:
+        out += "{C%s}" % (randint(2,13)) + c
+    say(where, out)
