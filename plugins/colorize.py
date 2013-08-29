07550d73f776b697a39e39f2657b50cbd4b6d3cf
Steven <theepicsnail@gmail.com>
2013-08-29 03:02:30 -0400

    Added colorize plugin

---
+++ plugins/colorize.py
@@ -0,0 +1,39 @@
+from . import *
+
+
+colors = ["{C0}", "{C4}", "{C7}", "{C3}", "{C10}", "{C6}"]
+clen = 6
+level = 0
+
+def up():
+    global level
+    level = (level + 1)%clen
+
+def down():
+    global level
+    level = (level + clen-1) %clen
+
+
+
+@command("colorize")
+def colorize(who, what, where):
+    global level
+    level = 0
+    out = colors[0]
+    for i in what:
+        if i == "(":
+            out += "("
+            up()
+            out += colors[level]
+        elif i == ",":
+            down()
+            out += colors[level] + ","
+            up()
+            out += colors[level]
+        elif i == ")":
+            down()
+            out += colors[level] + ")"
+        else:
+            out += i
+    say(where, out)
+
