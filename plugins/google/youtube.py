041272ab6c7aca36849e119e033899cea9161374
Steven <theepicsnail@gmail.com>
2013-08-30 12:03:10 -0400

    Added yt command

---
+++ plugins/google/youtube.py
@@ -0,0 +1,23 @@
+from .. import *
+import gdata.youtube
+import gdata.youtube.service
+import re
+
+_YT_SERVICE = gdata.youtube.service.YouTubeService()
+
+
+@command("yt")
+def yt(who, what, where):
+    query = gdata.youtube.service.YouTubeVideoQuery()
+    query.vq = what
+    query.max_results = 1
+    query.orderby = 'relevance'
+    query.racy = 'include'
+    feed = _YT_SERVICE.YouTubeQuery(query)
+
+    msg = "<{C4}Youtube{}: {B}%s{B} | {LINK}%s{}>" % (
+        feed.entry[0].media.title.text,
+        feed.entry[0].media.player.url
+    )
+
+    say(where, msg)
