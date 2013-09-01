6070df7a675b88e3c1692d4c1fdbd449480d1e31
Steven <theepicsnail@gmail.com>
2013-09-01 04:32:47 -0400

    Moved main2 to main

--- main.py
+++ main.py
@@ -1,91 +1,51 @@
-""" 
-    main.py -- main entry point for yakr
-    this doc string should be more helpful
-"""
-import argparse
-import yakr.config
-import yakr.plugin_manager
-import yakr.networking
-import yakr.connection
-import yakr.bot
-#import yakr.plugin_manager
-#import yakr.networking
-#def load_plugins(bot):
-#    """
-#    Scan for, and import all available plugins
-#    """
-#    logger.info("Starting plugin scan")
-#    for plugin in [ x for x in os.listdir('plugins') if "_" not in x ]:
-#        logger.info(plugin)
-#        module = __import__("plugins.%s" % plugin, {}, {}, [plugin])
-#        logger.info(module)
+from yakr.network import simple_connect, record, replay
+from yakr.util import set_procname
+from yakr.bot import Bot
+import yakr.config as botconfig
+import sys
+set_procname("yakr")
 
-_YAKR_DOC_ = """Yakr
----------------
-github.com/theepicsnail/Yakr
+config = botconfig.read("yakr.cfg")
 
-blah
-blah blah
-"""
-def main():
-    """ main """
-    parser = argparse.ArgumentParser(
-            formatter_class=argparse.RawDescriptionHelpFormatter,
-            description=_YAKR_DOC_)
+connect_host = config["connection"]["host"]
+connect_port = int(config["connection"]["port"])
+nick = config["bot"]["nick"]
+name = config["bot"]["name"]
 
-    parser.add_argument("-c", "--config",
-            metavar="config",
-            help="specify a config file (default: yakr.cfg)",
-            default="yakr.cfg")
 
-    parser.add_argument("action", 
-            choices=["run", "make_plugin", "record", "replay", "test"],
-            help="action to run",
-            default=0)
+net = None
+if len(sys.argv) == 2:
+    if sys.argv[1] == "record":
+        conn = simple_connect((connect_host, connect_port))
+        net = record(conn, "RECORD")
+    if sys.argv[1] == "replay":
+        net = replay("RECORD")
+else:
+    net = simple_connect((connect_host, connect_port))
 
-    parser.add_argument("plugins", 
-            metavar="plugin", 
-            nargs="*",
-            help="which plugins to use (default: all available)",
-            default=[None])
-
-    args = parser.parse_args()
-    config = yakr.config.read(args.config)
-    plugins = args.plugins
-    if plugins[0] is None:
-        plugins = yakr.plugin_manager.discover_plugins(config["plugin"]["path"])
-    globals()[args.action](config, plugins)
-
-def run(config, plugins):
-    """
-    Run the bot loading the specified plugins
-    if plugins is None, load all plugins possible 
-    """
-    bot = yakr.bot.Bot()
-    bot.load_config(config)
-    for plugin in plugins:
-        bot.load_plugin(plugin)
-    bot.run()
-
-def make_plugin(config, plugins):
-    raise Exception("Not implemented")
-
-def record(config, plugins):
-    raise Exception("Not implemented")
-
-def replay(config, plugins):
-    raise Exception("Not implemented")
-
-def test(config, plugins):
-    raise Exception("Not implemented")
-
-     
-if __name__ == '__main__':
-    main()
-
-    #Create the plugin manager
-    #Create the connection
-    #irc_bot = Irc.RealBot(SETTINGS)
-    #Bot.setPluginHandler(irc_bot) 
-    #irc_bot.start()
-    #irc_bot.wait()
+b = Bot(net)
+b.nick = nick
+b.real_name = name
+plugins = """alarm
+chance
+colorize
+date
+fortune
+gayify
+google.define
+google.search
+google.youtube
+invite
+joiner
+m528
+maths
+numbers
+ping
+repeater
+replacements
+sed
+tell
+titles
+weather""".split("\n")
+map(b.load, plugins)
+b.run()
