8c51017584ac5a747102d8e835f3fd35a0ddb3f3
Steven <theepicsnail@gmail.com>
2013-09-03 12:59:56 -0400

    Added twitter plugin

---
+++ plugins/twitter.py
@@ -0,0 +1,37 @@
+from . import *
+import tweepy
+
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+ACCESS_KEY = ""
+ACCESS_SECRET = ""
+CHANNELS = []
+
+execfile("twitter.cfg")
+
+_API = None
+
+def start():
+    global _API
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    _API = tweepy.API(auth)
+
+@command("")
+def manual_tweet(who, what, where):
+    print who, what, where
+    try:
+        if _API.update_status(what):
+            say(where, "Update {C9}posted")
+        else:
+            say(where, "Update {C5}failed")
+    except tweepy.TweepError as e:
+        say(where, "Update {C5}failed{}: " + e.message[0]["message"])
+
+#:snail!snail@airc-BD88CA3C TOPIC #test :changed topic
+@match("^[^ ]+ TOPIC (.*?) :(.*)$")
+def topic_tweet(groups):
+    where,what = groups
+    if where in CHANNELS:
+        manual_tweet("", where+": "+what, where)
+
