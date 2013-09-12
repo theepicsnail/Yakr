73d7f88d3c7d0776d3c22c3bc50b54b5839cffe6
Steven <theepicsnail@gmail.com>
2013-09-11 20:51:34 -0400

    Shortener now handles colored urls and errors better

--- plugins/shortener.py
+++ plugins/shortener.py
@@ -3,7 +3,7 @@ import urllib2
 import urllib
 import re
 
-_URL_RE = "(https?[^\s]*)"
+_URL_RE = "(https?[\x20-\x7F]+)"
 #http://v.gd/create.php?format=simple&url=www.example.com
 
 
@@ -15,8 +15,12 @@ def shorten(who, what, where):
 
     url = res.group(0)
     if len(url) <= 18:
-        return 
+        return
+    print "Shortening " + url
+    try:
+        content = urllib2.urlopen("http://v.gd/create.php?format=simple&url={}"
+            .format(urllib.quote(url))).read()
+        say(where, "<Shortened: {LINK}%s{} >" % content)
+    except urllib2.HTTPError as e:
+        say(where, e.read())
 
-    content = urllib2.urlopen("http://v.gd/create.php?format=simple&url={}"
-        .format(urllib.quote(url))).read()
-    say(where, "<Shortened: {LINK}%s{} >" % content)
