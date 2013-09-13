babc2763aea3f68b3b54ea3826902f627015191e
Steven <theepicsnail@gmail.com>
2013-09-13 03:04:55 -0400

    Forgot to redirect to pipes

--- plugins/github_reloader.py
+++ plugins/github_reloader.py
@@ -32,7 +32,7 @@ def on_privmsg(groups):
 @match(":{}.*PART {}".format(_BOT, _CHANNEL))
 def on_part(groups):
     pull_changes()
-    names = subprocess.Popen(["git", "diff", "--name-only", "HEAD", "HEAD~1"]).communicate()[0].strip().split("\n")
+    names = subprocess.Popen(["git", "diff", "--name-only", "HEAD", "HEAD~1"], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0].strip().split("\n")
     print "bot left"
     plugins = ""
     non_plugins = ""
