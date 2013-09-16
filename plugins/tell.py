1598e367c21195d1520c50ab8ed62bb0271ccce6
David Pflug <david@tpflug.com>
2013-09-16 08:56:23 -0400

    tell: Bugfix

--- plugins/tell.py
+++ plugins/tell.py
@@ -16,7 +16,7 @@ def stop():
     pickle.dump(TELL_CACHE, file(TELL_STORE, "w"))
 
 def now():
-    return datetime.datetime.now().strftime("%H:%M:%S")
+    return datetime.datetime.now()
 
 @command("tell")
 def set_tell(who, what, where):
@@ -25,7 +25,7 @@ def set_tell(who, what, where):
 
     nick, msg = what.split(" ", 1)    
     msg_list = TELL_CACHE.get(nick.lower(), [])
-    msg_list.append("<%s> %s: %s" % (now().strftime("%x %X"), who, msg))
+    msg_list.append("<{}> {}: {}".format(now().strftime("%x %X"), who, msg))
     TELL_CACHE[nick.lower()] = msg_list
 
 @privmsg
@@ -33,7 +33,7 @@ def check_tell(who, what, where):
     msgs = TELL_CACHE.get(who.lower(), [])
     if not msgs:
         return
-    say(where, "%s, you have %s message%s" % (
+    say(where, "{}, you have {} message{}".format(
         who,
         len(msgs),
         "." if len(msgs)==1 else "s."
