e73f574b64c2731bb21e9aaa6301e8b6831a242a
Steven <theepicsnail@gmail.com>
2013-09-27 12:01:03 -0400

    Removing debugging from google.search

--- plugins/google/search.py
+++ plugins/google/search.py
@@ -5,7 +5,6 @@ _API_URL = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q="
 
 @command("gs")
 def search(who, what, where):
-    print what
     reply_json = urllib2.urlopen(_API_URL + urllib2.quote(what)).read()
     response = json.loads(reply_json)
     data = response['responseData']
